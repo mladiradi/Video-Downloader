b90447353d006362a0446817059a5b93b1c1465f
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-09-06 12:51:47 +0300

    Update video_downloader.py
    def correction

--- video_downloader.py
+++ video_downloader.py
@@ -1,22 +1,35 @@
-
 import yt_dlp
+import os
 
-# Ask user to type in the link
-link = input("Enter the link of the YouTube video: ")
 
-# Define the download options
-ydl_opts = {
-    'format': 'bestvideo+bestaudio/best',  # Get the highest resolution video
-    'outtmpl': 'Downloads/python_mp4/%(title)s.%(ext)s',  # Specify download location
-    'merge_output_format': 'mp4'  # Merge video and audio into an mp4 file
-}
+def get_video_link():
+    return input("Enter the link of the YouTube video: ")
 
-# Show the message until downloading
-print("Downloading...")
 
-# Create the downloader object and download the video
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+def get_desktop_path():
+    return os.path.join(os.path.expanduser("~"), "Desktop")  # връща пътя до работния плот на потребителя
 
-# Show the message when download is completed
-print("Download completed!!")
+
+def download_video(link, download_path):
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',  # Взима най-доброто видео и аудио
+        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),  # Специфицира локацията на сваляне
+        'merge_output_format': 'mp4'  # Обединява видео и аудио в mp4 файл
+    }
+
+    print("Downloading...")
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+
+    print("Download completed!!")
+
+
+def main():
+    link = get_video_link()
+    desktop_path = get_desktop_path()
+    download_video(link, desktop_path)
+
+
+if __name__ == "__main__":
+    main()
